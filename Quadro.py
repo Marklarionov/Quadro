86c25999018ab35585afb9b22964178a31a256a2
Marklarionov <93324243+Marklarionov@users.noreply.github.com>
2022-02-01 14:07:02 +0200

    Update Quadro.py

--- Quadro.py
+++ Quadro.py
@@ -109,15 +109,102 @@ def kala():
     plt.grid(True)
     plt.show()
 
+def ochki():
+    x1= np.arange(-9,-0.5, 0.5)
+    y1=(-1/16)*(x1+5)**2+2
+    x2= np.arange(1, 9.5, 0.5)
+    y2=(-1/16)*(x2-5)**2+2
+    x3= np.arange(-9, -0.5, 0.5)
+    y3=(1/4)*(x3+5)**2-3
+    x4= np.arange(1, 9.5, 0.5)
+    y4=(1/4)*(x4-5)**2-3
+    x5= np.arange(-9, -5.5, 0.5)
+    y5=-(x5+7)**2+5
+    x6= np.arange(6, 9.5, 0.5)
+    y6=-(x6-7)**2+5
+    x7= np.arange(-1, 1.5, 0.5)
+    y7=-(0.5)*x7**2+1.5
+    fig=plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7)
+    plt.title("Очки")
+    plt.ylabel("y")
+    plt.xlabel("x")
+    plt.grid(True)
+    plt.show()
+
+def zont():
+    x1=np.arange(-12, 12.5, 0.5)
+    y1=(-1/18)*x1**2+12
+    x2=np.arange(-4, 4.5, 0.5)
+    y2=(-1/8)*x2**2+6
+    x3=np.arange(-12, -3.5, 0.5)
+    y3=(-1/8)*(x3+8)**2+6
+    x4=np.arange(4, 12.5, 0.5)
+    y4=(-1/8)*(x4-8)**2+6
+    x5=np.arange(-4, -0.3, 0.5)
+    y5=2*(x5+3)**2-9
+    x6=np.arange(-4, 0.2, 0.5)
+    y6=1.5*(x6+3)**2-10
+    fig=plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6)
+    plt.title("Зонт")
+    plt.ylabel("y")
+    plt.xlabel("x")
+    plt.grid(True)
+    plt.show()
+
+def baboch():
+    x1 = np.arange(-9, -0.5, 0.5)
+    y1 = -(1/8)*(x1+9)**2+8
+    x2 = np.arange(1, 9.5, 0.5)
+    y2 = -(1/8)*(x2-9)**2+8
+    x3 = np.arange(-9, -7.5, 0.5)
+    y3 = 7*(x3+8)**2+1
+    x4 = np.arange(8, 9.5, 0.5)
+    y4 = 7*(x4-8)**2+1
+    x5 = np.arange(-8, -0.5, 0.5)
+    y5 = 1/49*(x5+1)**2
+    x6 = np.arange(1, 8.5, 0.5)
+    y6=1/49*(x6-1)**2
+    x7 = np.arange(-8, -0.5, 0.5)
+    y7 = -4/49*(x7+1)**2
+    x8 = np.arange(1, 8.5, 0.5)
+    y8 = -4/49*(x8-1)**2
+    x9 = np.arange(-8, -1.5, 0.5)
+    y9 = 1/3*(x9+5)**2-7
+    x10 = np.arange(2, 8.5, 0.5)
+    y10 = 1/3*(x10-5)**2-7
+    x11 = np.arange(-2, -0.5, 0.5)
+    y11 = -2*(x11+1)**2-2
+    x12 = np.arange(1, 2.5, 0.5)
+    y12 = -2*(x12-1)**2-2
+    x13 = np.arange(-1, 1.5, 0.5)
+    y13 = -4*x13**2+2
+    x14 = np.arange(-1, 1.5, 0.5)
+    y14 = 4*x14**2-6
+    x15 = np.arange(-2, 0.5, 0.5)
+    y15 = -1.5*x15+2
+    x16 = np.arange(0, 2.5, 0.5)
+    y16 = 1.5*x16+2
+    fig = plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8,x9,y9,x10,y10,x11,y11,x12,y12,x13,y13,x14,y14,x15,y15,x16,y16)
+    plt.title('Бабочка')
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.grid(True)
+    plt.show()
+
 def figura():
     global var
     valik=var.get()
     if valik==1:
         kala()
     elif valik==2:
-        vihmavari()
+        ochki()
+    elif valik==3:
+        zont()
     else:
-        konn()
+        baboch()
 
 win=Tk()
 win.title("Квадратное уравнение")
@@ -150,15 +237,18 @@ but2=Button(f1,text="График",height=2,width=10,font="Arial 20",bg="Green",
 but2.place(x=730,y=125)
 
 btn_veel=Button(f2,text="Увеличить окно",font="Calibri 26",bg="green",command=veel)
-btn_veel.pack(side=TOP) 
+btn_veel.place(x=400,y=550) 
 var=IntVar()
 
-r1=Radiobutton(f2,text="Кит",variable=var,value=1, font="Calibri 26")#command=kala
-r2=Radiobutton(f2,text="Очки",variable=var,value=2, font="Calibri 26")
-r3=Radiobutton(f2,text="Лягуха",variable=var,value=3, font="Calibri 26")
+r1=Radiobutton(f2,text="Кит",variable=var,value=1, font="Calibri 26",command=figura)#command=kala
+r2=Radiobutton(f2,text="Очки",variable=var,value=2, font="Calibri 26",command=figura)
+r3=Radiobutton(f2,text="Зонт",variable=var,value=3, font="Calibri 26",command=figura)
+r4=Radiobutton(f2,text="Бабочк0",variable=var,value=4, font="Calibri 26",command=figura)
 r1.pack()
 r2.pack()
 r3.pack()
+r4.pack()
+
 Answer=Label(f1,text="",height=4,width=25,font="Arial 20",fg="Black",bg="Yellow",relief="solid")
 Answer.place(x=180,y=280)
 
